05ade662a579e63e2cfd42b16c10389a91456356
Noel Yohannan <noelyohan007@gmail.com>
2024-12-30 11:09:35 +0530

    chnaged theme using js

--- samp/static/js/script.js
+++ samp/static/js/script.js
@@ -1,18 +1,19 @@
 const sideMenu = document.querySelector('aside');
 const menuBtn = document.querySelector('#menu_bar');
-console.log(menuBtn);
 const closeBtn = document.querySelector('#close_btn');
 
 
-const themeToggler = document.querySelector('.theme-toggler');
+const themeToggler = document.querySelector('.theme_toggler');
 
-if (menuBtn) {
-    menuBtn.addEventListener('click', () => {
-        if (sideMenu) {
-            sideMenu.style.display = "block";
-        }
-    });
-} else {
-    console.error("Menu button not found!");
-}
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+})
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+})
 
+themeToggler.addEventListener('click',()=>{
+    document.body.classList.toggle('dark-theme-variables')
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active')
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active')
+})
